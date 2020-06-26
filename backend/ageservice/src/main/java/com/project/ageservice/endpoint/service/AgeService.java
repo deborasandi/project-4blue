dc523de58609d15b53b63fc52eed7daca8f81128
deborasandi <deborasandi@users.noreply.github.com>
2020-06-26 18:54:54 -0300

    reorganizando estrutura

---
+++ backend/ageservice/src/main/java/com/project/ageservice/endpoint/service/AgeService.java
@@ -0,0 +1,30 @@
+package com.project.ageservice.endpoint.service;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class AgeService {
+	class BirthDate {
+		String birthDate;
+
+		public String getBirthDate() {
+			return birthDate;
+		}
+
+		public void setBirthDate(String birthDate) {
+			this.birthDate = birthDate;
+		}
+	}
+	
+	public int calculate(String birthdate){
+		LocalDate birth = LocalDate.parse(birthdate);
+		
+        return Period.between(birth, LocalDate.now()).getYears();
+    }
+}
